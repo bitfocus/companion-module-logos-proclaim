37bb9bff77b2e4d0fd0592c7584e3eaeb7eb0a5b
Richard Huss <richard@huss.org.uk>
2026-03-24 21:21:20 +0000

    Fix up eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import { generateEslintConfig } from '@companion-module/tools/eslint/config.mjs'
+
+const baseConfig = await generateEslintConfig({})
+
+const customConfig = [
+    ...baseConfig,
+
+    {
+        languageOptions: {
+            sourceType: "module"
+        }
+    }
+]
+
+export default customConfig
