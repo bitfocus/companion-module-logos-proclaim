44e106ebc88ab2b498ec41197f7f217ec6002c8c
Richard Huss <richard@huss.org.uk>
2026-04-10 12:37:06 +0100

    Upgrade script to move password from config to secrets

--- src/upgrades.js
+++ src/upgrades.js
@@ -3,11 +3,17 @@ export const UpgradeScripts = [
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
 	 */
-	// function (context, props) {
-	// 	return {
-	// 		updatedConfig: null,
-	// 		updatedActions: [],
-	// 		updatedFeedbacks: [],
-	// 	}
-	// },
+	function (context, props) {
+		return {
+			updatedConfig: {
+				ip: context.currentConfig.ip,
+				// Password will be removed - moving to secrets
+			},
+			updatedSecrets: {
+				password: context.currentConfig.password,
+			},
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+	}
 ]
