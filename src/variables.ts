fd0c9decc22eadc7185c4883076ae2f74053bcc6
Richard Huss <richard@huss.org.uk>
2026-09-13 15:28:19 +0100

    Add a session ID variable

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,8 @@
 import type { ModuleInstance } from './main.js'
 
 export const UpdateVariableDefinitions = function (self: ModuleInstance): void {
-	self.setVariableDefinitions([{ variableId: 'on_air', name: 'On Air' }])
+	self.setVariableDefinitions([
+		{ variableId: 'on_air', name: 'On Air' },
+		{ variableId: 'session_id', name: 'Session ID' },
+	])
 }
